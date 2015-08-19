9eba81233b6b8d091bd20aea8cfef09f3a05d053
olivier <guerin.olivier@outlook.com>
2015-08-19 13:04:42 +0200

    base du projet

---
+++ configuration/build.js
@@ -0,0 +1,30 @@
+//mongo version >= 3.0
+//gather lng and lat in the same array "locations.geo":[<lng>,<lat>] and create a 2dsphere index.
+//arrays are preferred in mongo to store coordinates.
+
+var projection = {
+	'project_user': function project_user(){	
+		//We only want to change data that store both: lat and lng
+		var _users = db.users.find({'locations.lng':{$exists:true},'locations.lat':{$exists:true}});
+		while(_users.hasNext()){
+	 		var _user = _users.next();	 		
+         		_user.locations.geo = [];
+         		_user.locations.geo[0] = _user.locations.lng;
+	 		_user.locations.geo[1] = _user.locations.lat;
+  	 		delete _user.locations.lng;
+	 		delete _user.locations.lat;
+	 		var _r = db.users.save(_user);
+			_user.__writeResult = _r;
+	 		printjson(_user);
+		}
+		this.create_users_index();
+	},
+	'create_users_index':function create_users_index(){
+		//it is not ensureIndex in mongo 3.0
+		var _r = db.users.createIndex({'locations.geo':"2dsphere"});
+		printjson(_r);
+ 	}
+}
+
+
+
