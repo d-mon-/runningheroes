04c00c2759c4a5c7d268e94070f1835e8836fd80
olivier <guerin.olivier@outlook.com>
2015-08-19 14:57:13 +0200

    add logger step2

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+'use strict';
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var fs = require('fs');
+var logger = require('./logger.js');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var morgan = require('morgan');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,11 +19,13 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+
+app.use(morgan("combined",{"stream": logger.stream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-compass')({mode: 'expanded'}));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
