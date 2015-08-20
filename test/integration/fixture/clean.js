144669280b292e1a2bd4a4e3a061f30c30a6827d
olivier <guerin.olivier@outlook.com>
2015-08-20 16:46:51 +0200

    add fixture & clean

---
+++ test/integration/fixture/clean.js
@@ -0,0 +1,4 @@
+/**
+ * Created by GUERIN Olivier, on 20/08/2015.
+ * Twitter: @MisterRaton
+ */
---
+++ test/integration/fixture/users.js
@@ -0,0 +1,154 @@
+[
+  {
+    "email": "boris1@gmail.com",
+    "locations": {
+      "label": "Paris",
+      "lng": 2.3522219000000177,
+      "lat": 48.856614
+    }
+  },
+  {
+    "email": "stephane1@gmail.com",
+    "locations": {
+      "label": "Arras",
+      "lng": 2.7775349999999435,
+      "lat": 50.291002
+    }
+  },
+  {
+    "email": "maya1@gmail.com",
+    "locations": {
+      "lat": 50.725231,
+      "lng": 1.613334000000009,
+      "label": "Boulogne-sur-Mer"
+    }
+  },
+  {
+    "email": "nikola1@gmail.com",
+    "locations": {
+      "lat": 55.755826,
+      "lng": 37.6173,
+      "label": "?о?ква"
+    }
+  },
+  {
+    "email": "jerome1gmail.com",
+    "locations": {
+      "label": "Bondues",
+      "lng": 3.099695900000029,
+      "lat": 50.709069
+    }
+  },
+  {
+    "email": "maju1@gmail.com",
+    "locations": {
+      "label": "Zurich",
+      "lng": 8.539182500000038,
+      "lat": 47.3686498
+    }
+  },
+  {
+    "email": "marie1@orange.fr",
+    "locations": {
+      "label": "Boulogne-Billancourt",
+      "lng": 2.2399123000000145,
+      "lat": 48.8396952
+    }
+  },
+  {
+    "email": "pierre1@gmail.com",
+    "locations": {
+      "label": "Paris"
+    }
+  },
+  {
+    "email": "eric1@gmail.com",
+    "locations": {
+      "label": "Versailles",
+      "lng": 2.1301220000000285,
+      "lat": 48.801408
+    }
+  },
+  {
+    "email": "emir1@gmail.com",
+    "locations": {
+      "label": "Noisy-le-grand"
+    }
+  },
+  {
+    "email": "sylvain1@yahoo.fr",
+    "locations": {
+      "label": "Saint-Nazaire",
+      "lng": -2.2138479999999845,
+      "lat": 47.2734979
+    }
+  },
+  {
+    "email": "julien1@gmail.com",
+    "locations": {
+      "label": "Château-Thierry",
+      "lng": 3.40262000000007,
+      "lat": 49.0459869
+    }
+  },
+  {
+    "email": "yann1@gmail.com",
+    "locations": {
+      "label": "Gif-Sur-Yvette"
+    }
+  },
+  {
+    "email": "ben1@gmail.com",
+    "locations": {
+      "label": "Grenoble",
+      "lng": 5.724523999999974,
+      "lat": 45.188529
+    }
+  },
+  {
+    "email": "mlamaison1@gmail.fr",
+    "locations": {
+      "label": "Annecy",
+      "lng": 6.129383999999959,
+      "lat": 45.899247
+    }
+  },
+  {
+    "email": "thomas1@gmail.com",
+    "locations": {
+      "label": "Saint-?tienne",
+      "lng": 4.387177899999983,
+      "lat": 45.439695
+    }
+  },
+  {
+    "email": "sam1@gmail.com",
+    "locations": {
+      "label": "Saint-Yrieix-Sous-Aixe",
+      "lng": 1.0775790000000143,
+      "lat": 45.85824
+    }
+  },
+  {
+    "email": "axel1@gmail.com",
+    "locations": {
+      "label": "Paris",
+      "lng": 2.3522219000000177,
+      "lat": 48.856614
+    }
+  },
+  {
+    "email": "guillaume01@gmail.com",
+    "locations": {
+      "label": "Saint Erblon"
+    }
+  },
+  {
+    "email": "xibi1@gmail.com",
+    "locations": {
+      "label": "Lyon",
+      "lng": 4.835658999999964,
+      "lat": 45.764043
+    }
+  }
+]
